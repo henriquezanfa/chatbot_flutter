ecf4de027273ab94391485cf2b8a961534595555
Henrique <henriquezanferrari@gmail.com>
2021-12-01 20:39:26 -0300

    Adding theme configuration and fonts

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryLightColor = Color(0xffFFBA08);
+
+const accentLightColor = Color(0xffD00000);
+
+const backgroundLightColor = Color(0xfff8f8f8);
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chatbot/core/theme/colors.dart';
+
+final lightTheme = ThemeData(
+  primaryColor: primaryLightColor,
+  scaffoldBackgroundColor: backgroundLightColor,
+  textTheme: const TextTheme(
+    subtitle1: TextStyle(),
+    subtitle2: TextStyle(),
+    bodyText1: TextStyle(),
+    bodyText2: TextStyle(),
+    headline1: TextStyle(),
+    headline2: TextStyle(),
+    headline3: TextStyle(),
+    headline4: TextStyle(),
+    headline5: TextStyle(),
+    headline6: TextStyle(),
+    overline: TextStyle(),
+    caption: TextStyle(),
+    button: TextStyle(),
+  ).apply(
+    bodyColor: primaryLightColor,
+    fontFamily: 'Lato',
+  ),
+  colorScheme: const ColorScheme.dark(
+    primary: primaryLightColor,
+    secondary: accentLightColor,
+    background: backgroundLightColor,
+  ),
+);
